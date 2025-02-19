4c4cb30e0643b50b99a5c2912a506fd9e3c0c557
Josip Cerlek <cerlek.josip@gmail.com>
2025-02-19 15:21:41 +0100

    chore: reorganize .gitignore files and remove unused entries; adjust Navbar component styling

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return ( 
-        <div className="flex items-center p-6 shadow-lg shadow-blue-500/50 shadow-2xs m-5 rounded-xl h-16">
+        <div className="flex items-center p-6 shadow-lg  shadow-2xs m-5 rounded-xl h-16">
             <h1>SkyCast</h1>
         </div>
      );
