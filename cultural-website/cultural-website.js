cdc265141c21c2b510eb9104265ae9d2f6b107a1
Marija Palameta <palameta.marija610@gmail.com>
2025-02-21 17:35:52 +0100

    c

--- cultural-website/cultural-website.js
+++ cultural-website/cultural-website.js
@@ -33,5 +33,15 @@ window.addEventListener('scroll', () => {
 
 });
 
+ // Show/hide back to top button
+ window.onscroll = function() {
+    var button = document.querySelector('.back-to-top');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
 
     
