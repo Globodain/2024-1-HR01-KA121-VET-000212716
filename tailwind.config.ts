bf90c8105e7a39a12874bc5f56898865fd6f0746
Manuel <manuelradaljac@gmail.com>
2025-02-27 10:42:00 +0100

    sad radi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,32 +10,61 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: '#3273dc',
-          light: '#4287f5',
-          dark: '#2860b9',
-          50: '#eef5ff',
-          100: '#e0edff',
-          200: '#c7ddff',
-          300: '#9dc2ff',
-          400: '#729dff',
-          500: '#3273dc',
-          600: '#2860b9',
-          700: '#1e4b96',
-          800: '#1a3f7c',
-          900: '#162e59',
-        }
+          DEFAULT: '#1a29f9',
+          light: '#4652fa',
+          dark: '#1621c7',
+          50: '#eff1ff',
+          100: '#e0e3ff',
+          200: '#c1c7ff',
+          300: '#939cff',
+          400: '#6570ff',
+          500: '#1a29f9',
+          600: '#1621c7',
+          700: '#111a95',
+          800: '#0d1476',
+          900: '#09105a',
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       animation: {
         'marquee': 'marquee 30s linear infinite',
+        'marquee-slow': 'marquee 30s linear infinite',
+        'marquee-infinite': 'marquee 40s linear infinite',
       },
       keyframes: {
         marquee: {
           '0%': { transform: 'translateX(0)' },
           '100%': { transform: 'translateX(-100%)' },
-        },
+        }
       },
     },
   },
